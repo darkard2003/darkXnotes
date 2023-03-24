0dd6735d457a458572b046e0ba845f9d16b50d3d
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-03-24 22:38:45 +0530

    Fixed fingerprint

--- android/app/src/main/kotlin/com/darkchoclate/awesome_notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/darkchoclate/awesome_notes/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.darkchoclate.awesome_notes
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
